4e1785bec16edf221f5d92a969c52bfbfb57490a
datkadam <datkadam>
2024-12-01 01:44:05 +0530

    adding vpc infra

--- variable.tf
+++ variable.tf
@@ -1,21 +1,49 @@
-#Argument ka value datatype declare karte hai
+#argument ka value datatype declare karte hai 
 
-variable "region" {
-  type = string
- }
 
-variable "ami" {
-  type = string
-  default= "ami-03753afda9b8ba740"
-}
+
 
 variable "instance_type" {
-  type = string
-  default="t2.small"
+  type    = string
+  default = "t2.small"
 }
 
 variable "instance_count" {
-  description = "Number of instances to create"
-  type        = number
-  default     = 1
+  type = number
+}
+
+variable "aws_region" {
+  description = "AWS region where resources will be created"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "availability_zones" {
+  description = "List of availability zones for subnets"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "project_name" {
+  description = "Project name for tagging resources"
+  type        = string
+  default     = "MyVPCProject"
 }
